0c22daf96e3ff09959903d62d6684323c54b2e60
Anton <a.i.duda31@gmail.com>
2018-02-24 01:28:59 +0200

    Add bridging header.

---
+++ Anton_Duda_SimplePins/Anton_Duda_SimplePins/Anton_Duda_SimplePins-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Anton_Duda_SimplePins-Bridging-Header.h
+//  Anton_Duda_SimplePins
+//
+//  Created by Anton on 2/24/18.
+//  Copyright © 2018 Anton Duda. All rights reserved.
+//
+
+#ifndef Anton_Duda_SimplePins__Bridging_Header_h
+#define Anton_Duda_SimplePins__Bridging_Header_h
+
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+
+#endif /* Anton_Duda_SimplePins_Bridging_Header_h */
