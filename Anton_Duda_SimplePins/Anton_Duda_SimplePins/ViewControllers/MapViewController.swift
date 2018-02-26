fb6356f52ad1818ea0d3ac577696a9b21cd9efe5
Anton <a.i.duda31@gmail.com>
2018-02-26 12:44:37 +0200

    Added some changes to exstension to remove pins; Added UITapGestureRecognizer to add  and drag pins;

--- Anton_Duda_SimplePins/Anton_Duda_SimplePins/ViewControllers/MapViewController.swift
+++ Anton_Duda_SimplePins/Anton_Duda_SimplePins/ViewControllers/MapViewController.swift
@@ -21,7 +21,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         mapView.delegate = self
         mapView.register(MKPinAnnotationView.self, forAnnotationViewWithReuseIdentifier: "MKAnnotationView")
         
@@ -38,6 +38,11 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
                 mapView.addAnnotation(annotation)
             }
         }
+        
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action:#selector(self.addSomePin(_:)))
+        doubleTap.numberOfTapsRequired = 3
+        mapView.addGestureRecognizer(doubleTap)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -58,27 +63,16 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.requestWhenInUseAuthorization()
         }
     }
-   
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
-            print("locations = \(locValue.latitude) \(locValue.longitude)")
-        
-            centerMapOnLocation(location: locValue)
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = locValue
-            mapView.addAnnotation(annotation)
-    }
     
-    @IBAction func addSomePin(_ sender: UILongPressGestureRecognizer) {
-        let location = sender.location(in: mapView)
-        let locCoordinate = mapView.convert(location, toCoordinateFrom: mapView)
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
         
+        centerMapOnLocation(location: locValue)
         let annotation = MKPointAnnotation()
-        annotation.coordinate = locCoordinate
-        
-        self.mapView.addAnnotation(annotation)
-        DBManager.shared.addPin(with: locCoordinate.longitude, and: locCoordinate.latitude)
+        annotation.coordinate = locValue
+        mapView.addAnnotation(annotation)
     }
     
     @IBAction func logOutButton(_ sender: Any) {
@@ -91,13 +85,29 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             (UIApplication.shared.delegate as? AppDelegate)?.goToLogin()
         }
     }
+    @objc private func addSomePin(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: mapView)
+        let locCoordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = locCoordinate
+        
+        self.mapView.addAnnotation(annotation)
+        DBManager.shared.addPin(with: locCoordinate.longitude, and: locCoordinate.latitude)
+    }
 }
+
 
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView,
                  viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation.isKind(of: MKUserLocation.self) {return nil}
+        
         let view = mapView.dequeueReusableAnnotationView(withIdentifier: "MKAnnotationView") as? MKPinAnnotationView
         view?.isDraggable = true
+        view?.animatesDrop = true
+        view?.annotation = annotation
+        
         return view
     }
     
@@ -109,15 +119,19 @@ extension MapViewController: MKMapViewDelegate {
         case .starting:
             annotationOldCoordinates = view.annotation?.coordinate
         case .canceling:
+            guard let aoc = annotationOldCoordinates else {return}
             annotationOldCoordinates = nil
+            
+            guard let annotation = view.annotation else {return}
+            DBManager.shared.removePin(with: aoc.longitude, and: aoc.latitude)
+            
+            mapView.removeAnnotation(annotation)
         case .ending:
             guard let aoc = annotationOldCoordinates else {return}
             if let pin = DBManager.shared.getPint(with: aoc.longitude, and: aoc.latitude),
                 let newAnnotation = view.annotation {
-                
                 pin.latitude = newAnnotation.coordinate.latitude
                 pin.longitude = newAnnotation.coordinate.longitude
-                
                 DBManager.shared.saveContext()
             }
             annotationOldCoordinates = nil
@@ -126,3 +140,4 @@ extension MapViewController: MKMapViewDelegate {
         }
     }
 }
+
